64dab02229102fd07f92ff9698fc53b413e50096
benpu <pwc123451@gmail.com>
2022-09-27 22:31:49 -0400

    eeee

--- main.py
+++ main.py
@@ -9,7 +9,20 @@ board = [
     [1, 2, 0, 0, 0, 7, 4, 0, 0],
     [0, 4, 9, 2, 0, 6, 0, 0, 7]
 ]
+def valid(bo, num, pos):
+    #check row
+    for i in range(len(bo[0])):
+        if bo[pos[0]][i] == num and pos[1] != i:
+            return False
+    #check col
+    for i in range(len(bo)):
+        if bo[i][pos[1]] == num and pos[0] != i:
+            return False
+    #check box
+    box_x = pos[1] // 3
+    box_y = pos[0] // 3
 
+    for i in range(box_y * 3, box_y*3 + 3):
 
 def print_board(bo):
     for i in range(len(bo)):
