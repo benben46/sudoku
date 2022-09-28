2062e6a92aaca12350a708ccf8706148d2100e2f
benpu <pwc123451@gmail.com>
2022-09-28 01:10:17 -0400

    look at PEP8
    review the functions

--- main.py
+++ main.py
@@ -9,6 +9,27 @@ board = [
     [1, 2, 0, 0, 0, 7, 4, 0, 0],
     [0, 4, 9, 2, 0, 6, 0, 0, 7]
 ]
+
+
+def solve(bo):
+    find = find_empty(bo)
+    if not find:
+        return True
+    else:
+        row, col = find
+
+    for i in range(1, 10):
+        if valid(bo, i, (row, col)):
+            bo[row][col] = i
+
+            if solve(bo):
+                return True
+
+            bo[row][col] = 0
+
+    return False
+
+
 def valid(bo, num, pos):
     #check row
     for i in range(len(bo[0])):
@@ -23,6 +44,10 @@ def valid(bo, num, pos):
     box_y = pos[0] // 3
 
     for i in range(box_y * 3, box_y*3 + 3):
+        for j in range(box_x * 3, box_x * 3 + 3):
+            if bo[i][j] == num and (i, j) != pos:
+                return False
+
 
 def print_board(bo):
     for i in range(len(bo)):
